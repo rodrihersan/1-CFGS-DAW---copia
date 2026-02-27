950ad67e55fb64ecf72e384367f5178b40ffa956
Rodrigo <rodrihersan@gmail.com>
2026-02-27 10:43:52 +0100

    subida clase

--- EjemploJS/ejercicio3/js/javascript.js
+++ EjemploJS/ejercicio3/js/javascript.js
@@ -2,7 +2,7 @@ function holaMundo() {
 
     let dato = prompt("Introduzca su nombre por favor");
 
-    if (dato === null || dato.trim() === "") {
+    if (dato == null || dato == "") {
 
         console.error("No se ha introducido valor");
         alert("No se ha introducido valor");
@@ -18,4 +18,22 @@ function holaMundo() {
             console.warn("No se sabe su nombre");
         }
     }
+}
+
+function mostrarVariables() {
+
+    alert('Variable1: ' + variable1);
+
+    variable2 = null;
+    alert('Variable2: ' + variable2);
+
+    var variable1 = 34;
+    alert('Variable1: ' + variable1);
+
+    variable3 = 15.9;
+    alert('variable1 * variable3: ' + (variable1 * variable3));
+
+    variable3 = "ahora soy un texto";
+    alert('Variable3: ' + variable3);
+
 }
